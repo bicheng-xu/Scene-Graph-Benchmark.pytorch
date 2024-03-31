22facd9dac776221c061b122182ccec4290d2c76
bichengx <bicheng_xu@outlook.com>
2024-03-31 17:51:35 -0400

    set vector dirs

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -107,14 +107,14 @@ class DatasetCatalog(object):
             "ann_file": "cityscapes/annotations/instancesonly_filtered_gtFine_test.json"
         },
         "VG_stanford_filtered": {
-            "img_dir": "vg/VG_100K",
+            "img_dir": "/h/bichengx/site-pkgs/VG-SGG/VG_100K_3",
             "roidb_file": "vg/VG-SGG.h5",
             "dict_file": "vg/VG-SGG-dicts.json",
             "image_file": "vg/image_data.json",
         },
         "VG_stanford_filtered_with_attribute": {
-            "img_dir": "vg/VG_100K",
-            "roidb_file": "vg/VG-SGG-with-attri.h5",
+            "img_dir": "/h/bichengx/site-pkgs/VG-SGG/VG_100K_3",
+            "roidb_file": "/h/bichengx/site-pkgs/VG-SGG/VG-SGG-with-attri.h5",
             "dict_file": "vg/VG-SGG-dicts-with-attri.json",
             "image_file": "vg/image_data.json",
             "capgraphs_file": "vg/vg_capgraphs_anno.json",
