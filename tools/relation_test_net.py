73e0adb32402cd95c2eb97e2dc4e1fcd406ea3d0
bichengx <bicheng_xu@outlook.com>
2024-04-01 00:23:23 -0400

    update test saving dirs

--- tools/relation_test_net.py
+++ tools/relation_test_net.py
@@ -56,7 +56,9 @@ def main():
     cfg.merge_from_list(args.opts)
     cfg.freeze()
 
-    save_dir = ""
+    save_dir = os.path.join(cfg.OUTPUT_DIR, "inference_test_log")
+    if save_dir:
+        mkdir(save_dir)
     logger = setup_logger("maskrcnn_benchmark", save_dir, get_rank())
     logger.info("Using {} GPUs".format(num_gpus))
     logger.info(cfg)
@@ -99,7 +101,7 @@ def main():
 
     if cfg.OUTPUT_DIR:
         for idx, dataset_name in enumerate(dataset_names):
-            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference", dataset_name)
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference_test", dataset_name)
             mkdir(output_folder)
             output_folders[idx] = output_folder
     data_loaders_val = make_data_loader(cfg=cfg, mode="test", is_distributed=distributed, dataset_to_test=cfg.DATASETS.TO_TEST)
