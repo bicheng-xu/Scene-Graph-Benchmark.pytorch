b90f9e3eb633faa9218745b4ab248f1d90a10f36
bichengx <bicheng_xu@outlook.com>
2024-04-02 03:51:09 -0400

    update val scripts

--- vector-scripts/example-sgcls-val.sh
+++ vector-scripts/example-sgcls-val.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=example-sgcls-test
+#SBATCH --job-name=example-sgcls-val
 #SBATCH --nodes=1
 #SBATCH --partition=rtx6000
 #SBATCH --exclude=gpu138
-#SBATCH --mem=80GB
-#SBATCH -c 18
+#SBATCH --mem=40GB
+#SBATCH -c 9
 #SBATCH --gres=gpu:rtx6000:1
-#SBATCH --qos=normal
+#SBATCH --qos=m
 #SBATCH --time=12:00:00
 #SBATCH --export=ALL
 #SBATCH --open-mode=append
@@ -24,7 +24,7 @@ cd ${WORKSPACE}
 source /h/bichengx/site-pkgs/anaconda3/bin/activate SGed
 
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --master_port 10028 \
---nproc_per_node=1 tools/relation_test_net.py \
+--nproc_per_node=1 tools/relation_val_net.py \
 --config-file "configs/e2e_relation_X_101_32_8_FPN_1x.yaml" \
 MODEL.ROI_RELATION_HEAD.USE_GT_BOX True \
 MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL False \
@@ -36,4 +36,4 @@ TEST.IMS_PER_BATCH 1 \
 DTYPE "float16" \
 GLOVE_DIR /scratch/hdd001/home/bichengx/projects/SG-Models/glove \
 MODEL.PRETRAINED_DETECTOR_CKPT /scratch/hdd001/home/bichengx/projects/SG-Models/upload_causal_motif_sgcls/model_0032000.pth \
-OUTPUT_DIR /scratch/hdd001/home/bichengx/projects/SG-Models/upload_causal_motif_sgcls_test_2
+OUTPUT_DIR /scratch/hdd001/home/bichengx/projects/SG-Models/upload_causal_motif_sgcls_val
