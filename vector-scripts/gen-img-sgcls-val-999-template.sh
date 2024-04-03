c5e4db120de953aa916524a9f82064131db81073
bichengx <bicheng_xu@outlook.com>
2024-04-03 06:31:52 -0400

    update template

--- vector-scripts/gen-img-sgcls-val-999-template.sh
+++ vector-scripts/gen-img-sgcls-val-999-template.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=40GB
 #SBATCH -c 9
 #SBATCH --gres=gpu:rtx6000:1
-#SBATCH --qos=m
-#SBATCH --time=12:00:00
+#SBATCH --qos=m3
+#SBATCH --time=4:00:00
 #SBATCH --export=ALL
 #SBATCH --open-mode=append
 #SBATCH --output=slurm-output/%x.%j.out
@@ -23,8 +23,8 @@ cd ${WORKSPACE}
 
 source /h/bichengx/site-pkgs/anaconda3/bin/activate SGed
 
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --master_port 10028 \
---nproc_per_node=1 tools/relation_val_net.py \
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --master_port 11018 \
+--nproc_per_node=1 tools/relation_val_net_gen_img.py \
 --config-file "configs/e2e_relation_X_101_32_8_FPN_1x.yaml" \
 MODEL.ROI_RELATION_HEAD.USE_GT_BOX True \
 MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL False \
@@ -34,7 +34,13 @@ MODEL.ROI_RELATION_HEAD.CAUSAL.FUSION_TYPE sum \
 MODEL.ROI_RELATION_HEAD.CAUSAL.CONTEXT_LAYER motifs \
 TEST.IMS_PER_BATCH 1 \
 TEST.RELATION.IOU_THRESHOLD 0.999 \
+DATALOADER.ASPECT_RATIO_GROUPING False \
 DTYPE "float32" \
 GLOVE_DIR /scratch/hdd001/home/bichengx/projects/SG-Models/glove \
 MODEL.PRETRAINED_DETECTOR_CKPT /scratch/hdd001/home/bichengx/projects/SG-Models/upload_causal_motif_sgcls/model_0032000.pth \
-OUTPUT_DIR /scratch/hdd001/home/bichengx/projects/SG-Models/upload_causal_motif_sgcls_val-999
+DATA_STAT_DIR /scratch/hdd001/home/bichengx/projects/SG-Models/upload_causal_motif_sgcls \
+GEN_IMG.EVAL True \
+GEN_IMG.ANNO_DIR "/h/bichengx/site-pkgs/VG-SGG/V2-Last" \
+GEN_IMG.BASE_DIR "/scratch/hdd001/home/bichengx/projects/ControlNet/my-diff-cnet/vector-scripts-BX/val-scripts/val-output/vg-a40-single-run_0-1234" \
+GEN_IMG.FOLDER_NAME "validation_image_gt" \
+GEN_IMG.NUM_ROUNDS 5
