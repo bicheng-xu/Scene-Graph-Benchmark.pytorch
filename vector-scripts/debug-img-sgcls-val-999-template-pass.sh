b5862f0eb28130d8f12fdd5b8db7eb0e7926db64
bichengx <bicheng_xu@outlook.com>
2024-04-23 17:58:12 -0400

    update debug script

--- vector-scripts/debug-img-sgcls-val-999-template-pass.sh
+++ vector-scripts/debug-img-sgcls-val-999-template-pass.sh
@@ -1,29 +1,7 @@
-#!/bin/bash
-#SBATCH --job-name=example-sgcls-val-999-pass
-#SBATCH --nodes=1
-#SBATCH --partition=rtx6000
-#SBATCH --exclude=gpu138
-#SBATCH --mem=40GB
-#SBATCH -c 9
-#SBATCH --gres=gpu:rtx6000:1
-#SBATCH --qos=normal
-#SBATCH --time=2:00:00
-#SBATCH --export=ALL
-#SBATCH --open-mode=append
-#SBATCH --output=slurm-output/%x.%j.out
-#SBATCH --error=slurm-output/%x.%j.err
-#SBATCH --mail-user=bichengx@cs.ubc.ca
-#SBATCH --mail-type=ALL
-
-echo $(date) "--" ${SLURM_JOB_ID}
-module load my-cuda10.1+cudnn7.6.3
-
 WORKSPACE="/scratch/hdd001/home/bichengx/projects/Scene-Graph-Benchmark.pytorch"
 cd ${WORKSPACE}
 
 GEN_IMG_COMMON_PATH="/scratch/hdd001/home/bichengx/projects/ControlNet/my-diff-cnet/vector-scripts-BX/val-scripts/val-output"
-
-source /h/bichengx/site-pkgs/anaconda3/bin/activate SGed
 
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --master_port $1 \
 --nproc_per_node=1 tools/relation_val_net_gen_img.py \
