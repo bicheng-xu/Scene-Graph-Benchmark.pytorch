baaa8011abbc66326ebf2244b82828e49f94ac50
bichengx <bicheng_xu@outlook.com>
2024-04-23 20:30:22 -0400

    reorganize input args

--- vector-scripts/gen-img-sgcls-val-999-template-pass.sh
+++ vector-scripts/gen-img-sgcls-val-999-template-pass.sh
@@ -22,10 +22,14 @@ WORKSPACE="/scratch/hdd001/home/bichengx/projects/Scene-Graph-Benchmark.pytorch"
 cd ${WORKSPACE}
 
 GEN_IMG_COMMON_PATH="/scratch/hdd001/home/bichengx/projects/ControlNet/my-diff-cnet/vector-scripts-BX/val-scripts/val-output"
+PORT_NUM=$1
+GEN_IMG_ANNO_FILE=$2
+GEN_IMG_BASE_DIR=$3
+GEN_IMG_FOLDER_NAME=$4
 
 source /h/bichengx/site-pkgs/anaconda3/bin/activate SGed
 
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --master_port $1 \
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --master_port ${PORT_NUM} \
 --nproc_per_node=1 tools/relation_val_net_gen_img.py \
 --config-file "configs/e2e_relation_X_101_32_8_FPN_1x.yaml" \
 MODEL.ROI_RELATION_HEAD.USE_GT_BOX True \
@@ -43,7 +47,7 @@ MODEL.PRETRAINED_DETECTOR_CKPT /scratch/hdd001/home/bichengx/projects/SG-Models/
 DATA_STAT_DIR /scratch/hdd001/home/bichengx/projects/SG-Models/upload_causal_motif_sgcls \
 GEN_IMG.EVAL True \
 GEN_IMG.ANNO_DIR "/h/bichengx/site-pkgs/VG-SGG/V2-Last" \
-GEN_IMG.ANNO_FILE $2 \
-GEN_IMG.BASE_DIR "${GEN_IMG_COMMON_PATH}/$3" \
-GEN_IMG.FOLDER_NAME $4 \
+GEN_IMG.ANNO_FILE ${GEN_IMG_ANNO_FILE} \
+GEN_IMG.BASE_DIR "${GEN_IMG_COMMON_PATH}/${GEN_IMG_BASE_DIR}" \
+GEN_IMG.FOLDER_NAME ${GEN_IMG_FOLDER_NAME} \
 GEN_IMG.NUM_ROUNDS 5
