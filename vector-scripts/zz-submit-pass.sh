e0cd0e278ee361fc117a76b18ea29ea3ce264439
bichengx <bicheng_xu@outlook.com>
2024-04-30 15:27:16 -0400

    update

--- vector-scripts/zz-submit-pass.sh
+++ vector-scripts/zz-submit-pass.sh
@@ -1,5 +1,23 @@
 echo 1
-sbatch gen-img-sgcls-val-999-template-pass.sh 11011 "validation_data_bbox_dbox32_np.pkl" "vg-norel-prompt-object-random-50imagebboxdrop50-lossreweight-maskingonlyadd-a40-object-original-iter_48200-run_0-1234" "generated_image"
+sbatch gen-img-sgcls-val-999-template-pass.sh 11011 "validation_data_bbox_dbox32_np.pkl" "vg-norel-prompt-object-random-a40-object-original-iter_144600-run_0-1234" "generated_image"
 sleep 1s
 echo 2
-sbatch gen-img-sgcls-val-999-template-pass.sh 11021 "validation_data_bbox_dbox32_np.pkl" "vg-norel-prompt-object-random-50imagebboxdrop50-lossreweight-maskingonlyadd-a40-object-ordered-iter_48200-run_0-1234" "generated_image"
+sbatch gen-img-sgcls-val-999-template-pass.sh 11021 "validation_data_bbox_dbox32_np.pkl" "vg-norel-prompt-object-random-a40-object-ordered-iter_144600-run_0-1234" "generated_image"
+sleep 1s
+echo 3
+sbatch gen-img-sgcls-val-999-template-pass.sh 11031 "validation_data_bbox_dbox32_np.pkl" "vg-prompt-object-random-a40-object-original-iter_144600-run_0-1234" "generated_image"
+sleep 1s
+echo 4
+sbatch gen-img-sgcls-val-999-template-pass.sh 11041 "validation_data_bbox_dbox32_np.pkl" "vg-prompt-object-random-a40-object-ordered-iter_144600-run_0-1234" "generated_image"
+sleep 1s
+echo 5
+sbatch gen-img-sgcls-val-999-template-pass.sh 11051 "validation_data_bbox_dbox32_np.pkl" "vg-norel-prompt-object-random-qi-a40-object-original-iter_144600-run_0-1234" "generated_image"
+sleep 1s
+echo 6
+sbatch gen-img-sgcls-val-999-template-pass.sh 11061 "validation_data_bbox_dbox32_np.pkl" "vg-prompt-object-random-qi-a40-object-original-iter_144600-run_0-1234" "generated_image"
+sleep 1s
+echo 7
+sbatch gen-img-sgcls-val-999-template-pass.sh 11071 "validation_data_bbox_dbox32_np.pkl" "vg-norel-prompt-object-random-qi-a40-object-original-iter_192800-run_0-1234" "generated_image"
+sleep 1s
+echo 8
+sbatch gen-img-sgcls-val-999-template-pass.sh 11081 "validation_data_bbox_dbox32_np.pkl" "vg-prompt-object-random-qi-a40-object-original-iter_192800-run_0-1234" "generated_image"
