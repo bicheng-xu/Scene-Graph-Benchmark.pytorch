04cf260c65913f1d3cb252fb5a06ca04b6a1f027
bichengx <bicheng_xu@outlook.com>
2024-06-26 23:11:12 -0400

    update

--- vector-scripts/gen-img-vctree-sgcls-val-999-template-pass.sh
+++ vector-scripts/gen-img-vctree-sgcls-val-999-template-pass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=example-sgcls-val-999-pass
+#SBATCH --job-name=example-vctree-sgcls-val-999-pass
 #SBATCH --nodes=1
 #SBATCH --partition=rtx6000
 #SBATCH --exclude=gpu138,gpu169
@@ -38,17 +38,14 @@ CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --master_port ${PORT_N
 --config-file "configs/e2e_relation_X_101_32_8_FPN_1x.yaml" \
 MODEL.ROI_RELATION_HEAD.USE_GT_BOX True \
 MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL False \
-MODEL.ROI_RELATION_HEAD.PREDICTOR CausalAnalysisPredictor \
-MODEL.ROI_RELATION_HEAD.CAUSAL.EFFECT_TYPE TDE \
-MODEL.ROI_RELATION_HEAD.CAUSAL.FUSION_TYPE sum \
-MODEL.ROI_RELATION_HEAD.CAUSAL.CONTEXT_LAYER motifs \
+MODEL.ROI_RELATION_HEAD.PREDICTOR VCTreePredictor \
 TEST.IMS_PER_BATCH 1 \
 TEST.RELATION.IOU_THRESHOLD 0.999 \
 DATALOADER.ASPECT_RATIO_GROUPING False \
 DTYPE "float32" \
 GLOVE_DIR /scratch/hdd001/home/bichengx/projects/SG-Models/glove \
-MODEL.PRETRAINED_DETECTOR_CKPT /scratch/hdd001/home/bichengx/projects/SG-Models/upload_causal_motif_sgcls/model_0032000.pth \
-DATA_STAT_DIR /scratch/hdd001/home/bichengx/projects/SG-Models/upload_causal_motif_sgcls \
+MODEL.PRETRAINED_DETECTOR_CKPT /scratch/hdd001/home/bichengx/projects/SG-Models/vctree-sgcls_again_upload/model_0036000.pth \
+DATA_STAT_DIR /scratch/hdd001/home/bichengx/projects/SG-Models/vctree-sgcls_again_upload \
 GEN_IMG.EVAL True \
 GEN_IMG.ANNO_DIR "/h/bichengx/site-pkgs/VG-SGG/V2-Last" \
 GEN_IMG.ANNO_FILE ${GEN_IMG_ANNO_FILE} \
